794fe043be6b92ab78b0f774f6e5464a43b46891
Ishank Mittal <mittalishank@gmail.com>
2026-06-04 19:40:32 +0530

    Update backed main.py

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,9 @@ app = FastAPI(title='Safety Detection API', version='1.0')
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=["https://dashboard-nine-ebon-70.vercel.app",
+        "http://localhost:3000"],
+    allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*']
 )
